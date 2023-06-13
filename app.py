b1b0bc99331a655ab085e7593e9e09fc9cbd27ee
12hema <95902392+12hema@users.noreply.github.com>
2023-06-13 20:37:30 +0530

    app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ model = pickle.load(open('classifier.pkl', 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['GET'])
+@app.route('/predict',methods=['POST'])
 def predict():
     
     float_features = [float(x) for x in request.form.values()]
